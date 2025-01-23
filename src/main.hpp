81202904ce394e9a4db1b0d6e439049b6a98640f
Alon A. Rabinowitz <alonrab@outlook.com>
2025-01-23 13:31:33 +0200

    add main.hpp file and update platformio.ini for U8g2 library

---
+++ src/main.hpp
@@ -0,0 +1,24 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+#define VERBOSE
+// #define RECIVER_MODE
+// #define SENDER_MODE
+#define SEND_RECIVE_MODE
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+struct _datapacket
+{
+    String message;
+    unsigned char value;
+};
+
+enum Mode
+{
+    MASTER = 0,
+    SLAVE,
+    OTHER
+};
+
+#endif
